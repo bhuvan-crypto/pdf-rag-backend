3b636e1925cbf84cb2742d9a238e434f9448b8ce
bhuvan-crypto <55475746+bhuvan-crypto@users.noreply.github.com>
2025-12-16 07:47:54 +0000

    fix

--- main.py
+++ main.py
@@ -17,15 +17,10 @@ load_dotenv()
 
 app = FastAPI(root_path="/api-python")
 
-origins = [
-    "https://bhuvan-se.duckdns.org",
-    "http://localhost:3000",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
